b08f49e094c9b71badf1dfb4e0a21a9de264ed8e
Pratyush Das <reikdas@gmail.com>
2023-11-16 11:49:54 -0500

    C codegen

--- test.py
+++ test.py
@@ -1,27 +1,58 @@
 def codegen(x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre):
-    print(f"""
-x = {x}
-y = [0] * {len(x)}
-val = {val}
-indx = {indx}
-bindx = {bindx}
-rpntr = {rpntr}
-cpntr = {cpntr}
-bpntrb = {bpntrb}
-bpntre = {bpntre}
-    """)
+    print("#include <stdio.h>\n")
+    print("int main() {")
+    print(f"\tint y[{len(x)}] = {{0}};")
+    print(f"\tint x[{len(x)}] = {{{','.join(map(str, x))}}};")
+    print(f"\tint val[] = {{{','.join(map(str, val))}}};")
+    print(f"\tint indx[] = {{{','.join(map(str, indx))}}};")
+    print(f"\tint bindx[] = {{{','.join(map(str, bindx))}}};")
+    print(f"\tint rpntr[] = {{{','.join(map(str, rpntr))}}};")
+    print(f"\tint cpntr[] = {{{','.join(map(str, cpntr))}}};")
+    print(f"\tint bpntrb[] = {{{','.join(map(str, bpntrb))}}};")
+    print(f"\tint bpntre[] = {{{','.join(map(str, bpntre))}}};")
+    print("\tint count = 0;\n")
     count = 0
     for a in range(len(rpntr)-1):
         valid_cols = bindx[bpntrb[a]-bpntrb[0]:bpntre[a]-bpntrb[0]]
         for b in range(len(cpntr)-1):
             if (b+1) in valid_cols:
-                print("count2 = 0")
-                print(f"for j in range({cpntr[b]-1}, {cpntr[b+1]-1}):")
-                print(f"\tfor i in range({rpntr[a]-1}, {rpntr[a+1]-1}):")
-                print(f"\t\ty[i] += val[{indx[count]-1}+count2] * x[j]")
-                print("\t\tcount2+=1")
+                print("\tcount = 0;")
+                print(f"\tfor (int j = {cpntr[b]-1}; j < {cpntr[b+1]-1}; j++) {{")
+                print(f"\t\tfor (int i = {rpntr[a]-1}; i < {rpntr[a+1]-1}; i++) {{")
+                print(f"\t\t\ty[i] += val[{indx[count]-1}+count] * x[j];")
+                print("\t\t\tcount++;")
+                print("\t\t}")
+                print("\t}")
                 count+=1
-    print("print(y)")
+    print(f"\tfor (int i=0; i<{len(x)}; i++) {{")
+    print("\t\tprintf(\"%d \", y[i]);")
+    print("\t}\n")
+    print("\tprintf(\"\\n\");\n")
+    print("}\n")
+
+#     print(f"""
+# x = {x}
+# y = [0] * {len(x)}
+# val = {val}
+# indx = {indx}
+# bindx = {bindx}
+# rpntr = {rpntr}
+# cpntr = {cpntr}
+# bpntrb = {bpntrb}
+# bpntre = {bpntre}
+#     """)
+#     count = 0
+#     for a in range(len(rpntr)-1):
+#         valid_cols = bindx[bpntrb[a]-bpntrb[0]:bpntre[a]-bpntrb[0]]
+#         for b in range(len(cpntr)-1):
+#             if (b+1) in valid_cols:
+#                 print("count2 = 0")
+#                 print(f"for j in range({cpntr[b]-1}, {cpntr[b+1]-1}):")
+#                 print(f"\tfor i in range({rpntr[a]-1}, {rpntr[a+1]-1}):")
+#                 print(f"\t\ty[i] += val[{indx[count]-1}+count2] * x[j]")
+#                 print("\t\tcount2+=1")
+#                 count+=1
+#     print("print(y)")
 
 
 if __name__ == "__main__":

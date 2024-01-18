d6152fcd7664dd07d035ee31e21a553f9036ac62
Pratyush Das <reikdas@gmail.com>
2024-01-18 09:40:14 -0500

    Reorder loops in generated code

--- vbrgen.py
+++ vbrgen.py
@@ -86,11 +86,9 @@ def spmv_codegen():
                 valid_cols = bindx[bpntrb[a]:bpntre[a]]
                 for b in range(len(cpntr)-1):
                     if b in valid_cols:
-                        f.write("\tcount = 0;\n")
-                        f.write(f"\tfor (int j = {cpntr[b]}; j < {cpntr[b+1]}; j++) {{\n")
-                        f.write(f"\t\tfor (int i = {rpntr[a]}; i < {rpntr[a+1]}; i++) {{\n")
-                        f.write(f"\t\t\ty[i] += val[{indx[count]}+count] * x[j];\n")
-                        f.write("\t\t\tcount++;\n")
+                        f.write(f"\tfor (int i = {rpntr[a]}; i < {rpntr[a+1]}; i++) {{\n")
+                        f.write(f"\t\tfor (int j = {cpntr[b]}; j < {cpntr[b+1]}; j++) {{\n")
+                        f.write(f"\t\t\ty[i] += val[{indx[count]}+(j-{cpntr[b]})*{rpntr[a+1]} + i] * x[j];\n")
                         f.write("\t\t}\n")
                         f.write("\t}\n")
                         count+=1

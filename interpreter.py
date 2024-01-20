8b55320150a729c40e58ee4bced8f5dd8017f014
Pratyush Das <reikdas@gmail.com>
2024-01-19 19:44:58 -0500

    Add pseudocode for future interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
 from vbrgen import read_data
+from functools import partial
 
 def interpret(filename):
     x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre = read_data(filename)
@@ -16,4 +17,14 @@ def interpret(filename):
                         y[i] += val[indx[count1] + count2] * x[j]
                         count2 += 1
                 count1 += 1
+
+                # def spMVDot(denseBlock):
+                #     def vectorDot(i):
+                #         ewise_product: [cpntr[b+1] - cpntr[b]] = build(cpntr[b], cpntr[b+1], lambda j: val[indx[denseBlock]+(j-cpntr[b])*rpntr[a+1]+i] * x[j])
+                #         res: float = fold(len(ewise_product), lambda carry, idx: carry + ewise_product[idx], init_carry=0)
+                #         return res
+
+                #     return build(rpntr[a], rpntr[a+1], vectorDot) # y
+
+                # build(0, len(indx), spMVDot)
     return y

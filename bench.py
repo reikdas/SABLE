c4022c2c0eb6f75aa039c25df207dc8c6621ffad
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-01-25 14:40:37 -0500

    bench.py: friendlier names of file handles

--- bench.py
+++ bench.py
@@ -6,24 +6,24 @@ from src.spmv_codegen import spmv_codegen
 
 if __name__ == "__main__":
     d = spmv_codegen(bench=True)
-    with open("benchmarks_my.txt", "w") as f1:
-        with open("benchmarks_dense.txt", "w") as f2:
-            with open("benchmarks_inspector.txt", "w") as f3:
+    with open("benchmarks_my.txt", "w") as fMy:
+        with open("benchmarks_dense.txt", "w") as fDense:
+            with open("benchmarks_inspector.txt", "w") as fInspector:
                 for filename in os.listdir("Generated_SpMV"):
                     if filename.endswith(".c"):
-                        f1.write(f"{filename[:-2]}")
+                        fMy.write(f"{filename[:-2]}")
                         print(filename)
                         print("Benchmarking mine")
                         time1 = time.time_ns() // 1_000_000
                         subprocess.run(["gcc", "-O3", "-o", filename[:-2], filename], cwd="Generated_SpMV")
                         time2 = time.time_ns() // 1_000_000
                         diff = time2 - time1
-                        f3.write(f"{filename[:-2]},{d[filename[:-2]]+diff}\n")
+                        fInspector.write(f"{filename[:-2]},{d[filename[:-2]]+diff}\n")
                         subprocess.call(["./"+filename[:-2]], stdout=subprocess.PIPE, cwd="Generated_SpMV")
                         for i in range(5):
                             output = subprocess.run(["./"+filename[:-2]], capture_output=True, cwd="Generated_SpMV")
-                            f1.write(","+output.stdout.decode("utf-8").split("\n")[0].split(" = ")[1])
-                        f1.write("\n")
+                            fMy.write(","+output.stdout.decode("utf-8").split("\n")[0].split(" = ")[1])
+                        fMy.write("\n")
                         # print("Benchmarking dense")
                         # mtx_file = os.path.join("Generated_Matrix", filename[:-2]+".mtx")
                         # subprocess.run(["gcc", "-O3", "-o", filename[:-2], "dense.c", "-lcblas"])
@@ -33,4 +33,4 @@ if __name__ == "__main__":
                         #     output = subprocess.run(["./"+filename[:-2], mtx_file], capture_output=True)
                         #     sum += float(output.stdout.decode("utf-8").split("\n")[0].split(" = ")[1])
                         # avg = sum/5
-                        # f2.write(f"{filename[:-2]}: {avg}ms\n")
+                        # fDense.write(f"{filename[:-2]}: {avg}ms\n")

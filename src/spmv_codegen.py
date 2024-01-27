70feca89df68f8eca09f4d72d87f43efeaff3239
Pratyush Das <reikdas@gmail.com>
2024-01-27 01:39:13 -0500

    Inefficient parallelization works

--- src/spmv_codegen.py
+++ src/spmv_codegen.py
@@ -25,6 +25,12 @@ def vbr_spmv_codegen(filename: str, dir_name = "Generated_SpMV"):
     filename = filename[:-5]
 
     time1 = time.time_ns() // 1_000
+    # Find size of auxillary array
+    largest_diff = rpntr[1] - rpntr[0]
+    for a in range(len(rpntr)-1):
+        current_diff = rpntr[a+1] - rpntr[a]
+        if current_diff > largest_diff:
+            largest_diff = current_diff
     with open(os.path.join(dir_name, filename+".c"), "w") as f:
         f.write("#include <stdio.h>\n")
         f.write("#include <sys/time.h>\n")
@@ -56,10 +62,12 @@ def vbr_spmv_codegen(filename: str, dir_name = "Generated_SpMV"):
     if(fscanf(file, \"%c\", &c));
     assert(c=='\\n');\n'''.format(variable))
         f.write("\tfclose(file);\n")
-        f.write("\tint count = 0;\n")
         f.write("\tstruct timeval t1;\n")
         f.write("\tgettimeofday(&t1, NULL);\n")
         f.write("\tlong t1s = t1.tv_sec * 1000000L + t1.tv_usec;\n")
+        f.write("\tint n = omp_get_max_threads();\n")
+        f.write("\tint thread_id;\n")
+        f.write(f"\tint aux[{largest_diff}*n];\n")
         count = 0
         for a in range(len(rpntr)-1):
             if bpntrb[a] == -1:
@@ -67,21 +75,29 @@ def vbr_spmv_codegen(filename: str, dir_name = "Generated_SpMV"):
             valid_cols = bindx[bpntrb[a]:bpntre[a]]
             for b in range(len(cpntr)-1):
                 if b in valid_cols:
+                    f.write(f"\tmemset(aux, 0, sizeof(aux));\n")
+                    f.write(f"\t#pragma omp parallel for\n")
                     f.write(f"\tfor (int j = {cpntr[b]}; j < {cpntr[b+1]}; j++) {{\n")
-                    f.write(f"\t\t#pragma omp parallel for reduction(+:y[:{rpntr[a+1]}-{rpntr[a]}])\n")
+                    f.write(f"\t\tthread_id = omp_get_thread_num();\n")
                     f.write(f"\t\tfor (int i = {rpntr[a]}; i < {rpntr[a+1]}; i++) {{\n")
-                    f.write(f"\t\t\ty[i] += val[{indx[count]}+(j-{cpntr[b]})*({rpntr[a+1]} - {rpntr[a]}) + (i-{rpntr[a]})] * x[j];\n")
+                    f.write(f"\t\t\taux[(i-{rpntr[a]}) + ({rpntr[a+1]} - {rpntr[a]})*thread_id] += val[{indx[count]}+(j-{cpntr[b]})*({rpntr[a+1]} - {rpntr[a]}) + (i-{rpntr[a]})] * x[j];\n")
+                    f.write("\t\t}\n")
+                    f.write("\t}\n")
+                    f.write(f"\tfor (int i={rpntr[a]}; i<{rpntr[a+1]}; i++) {{\n")
+                    f.write(f"\t\tfor (int j=0; j<n; j++) {{\n")
+                    f.write(f"\t\t\ty[i] += aux[(i-{rpntr[a]})+({rpntr[a+1]} - {rpntr[a]})*j];\n")
                     f.write("\t\t}\n")
                     f.write("\t}\n")
                     count+=1
         f.write("\tstruct timeval t2;\n")
         f.write("\tgettimeofday(&t2, NULL);\n")
         f.write("\tlong t2s = t2.tv_sec * 1000000L + t2.tv_usec;\n")
+        f.write("\tfree(x);\n")
+        f.write("\tfree(val);\n")
         f.write("\tprintf(\"{0} = %lu\\n\", t2s-t1s);\n".format(filename))
         f.write(f"\tfor (int i=0; i<{len(x)}; i++) {{\n")
         f.write("\t\tprintf(\"%.2f\\n\", y[i]);\n")
         f.write("\t}\n")
         f.write("}\n")
-
         time2 = time.time_ns() // 1_000
     return time2-time1

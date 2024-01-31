f796d0fda5e495b84ca0ee1b8fae68dd53102f9d
Pratyush Das <reikdas@gmail.com>
2024-01-31 14:34:21 -0500

    Single threaded should also gen floats

--- src/spmv_codegen.py
+++ src/spmv_codegen.py
@@ -21,6 +21,8 @@ def vbr_spmv_codegen_for_all(threads=16):
     return runtimes
         
 def gen_single_threaded(x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre, dir_name, filename, rel_path):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
     with open(os.path.join(dir_name, filename+".c"), "w") as f:
         f.write("#include <stdio.h>\n")
         f.write("#include <sys/time.h>\n")
@@ -29,19 +31,19 @@ def gen_single_threaded(x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre, dir_n
         f.write("int main() {\n")
         f.write(f"\tFILE *file = fopen(\"{os.path.abspath(rel_path)}\", \"r\");\n")
         f.write("\tif (file == NULL) { printf(\"Error opening file\"); return 1; }\n")
-        f.write(f"\tint* y = (int*)calloc({len(x)}, sizeof(int));\n")
-        f.write(f"\tint* x = (int*)calloc({len(x) + 1}, sizeof(int));\n")
-        f.write(f"\tint* val = (int*)calloc({len(val) + 1}, sizeof(int));\n")
+        f.write(f"\tfloat* y = (float*)calloc({len(x)}, sizeof(int));\n")
+        f.write(f"\tfloat* x = (float*)calloc({len(x) + 1}, sizeof(int));\n")
+        f.write(f"\tfloat* val = (float*)calloc({len(val) + 1}, sizeof(int));\n")
         f.write("\tchar c;\n")
         f.write(f"\tint x_size=0, val_size=0;\n")
         for variable in ["x", "val"]:
             f.write('''
-    assert(fscanf(file, "{0}=[%d", &{0}[{0}_size]) == 1);
+    assert(fscanf(file, "{0}=[%f", &{0}[{0}_size]) == 1);
     {0}_size++;
     while (1) {{
         assert(fscanf(file, \"%c\", &c) == 1);
         if (c == ',') {{
-            assert(fscanf(file, \"%d\", &{0}[{0}_size]) == 1);
+            assert(fscanf(file, \"%f\", &{0}[{0}_size]) == 1);
             {0}_size++;
         }} else if (c == ']') {{
             break;
@@ -74,11 +76,14 @@ def gen_single_threaded(x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre, dir_n
         f.write("\tlong t2s = t2.tv_sec * 1000000L + t2.tv_usec;\n")
         f.write("\tprintf(\"{0} = %lu\\n\", t2s-t1s);\n".format(filename))
         f.write(f"\tfor (int i=0; i<{len(x)}; i++) {{\n")
-        f.write("\t\tprintf(\"%d\\n\", y[i]);\n")
+        f.write("\t\tprintf(\"%f\\n\", y[i]);\n")
         f.write("\t}\n")
         f.write("}\n")
 
 def gen_multi_threaded(threads, x, val, indx, bindx, rpntr, cpntr, bpntrb, bpntre, dir_name, filename, rel_path):
+    dir_name = "Generated_SpMV"
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
     with open(os.path.join(dir_name, filename+".c"), "w") as f:
         f.write("#include <stdio.h>\n")
         f.write("#include <sys/time.h>\n")

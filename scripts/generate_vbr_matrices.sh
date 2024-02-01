b9b3a110933e80dae193e95686662746011e3ac8
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-01-31 21:50:24 -0500

    generate VBR matrices with GNU parallel
    this allows to specify number of jobs: see the `jobs` variable in
    scripts/generate_vbr_matrices.sh

--- scripts/generate_vbr_matrices.sh
+++ scripts/generate_vbr_matrices.sh
@@ -1,54 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rows=(1000)
-cols=(1000)
-partition_types=(uniform non-uniform)
-row_splits=(50 100)
-col_splits=(50 100)
-percentage_of_blocks=(20 15 10 5 1)
-percentage_of_zeros=(50 40 30 20 10 0)
+rows=100
+cols=100
+partition_types="uniform non-uniform"
+row_splits="50 100"
+col_splits="50 100"
+percentage_of_blocks="20 15 10 5 1"
+percentage_of_zeros="50 40 30 20 10 0"
 FAIL=0
+
+jobs=2
 
 # time execution of script
 start=`date +%s.%N`
-for row in "${rows[@]}"
-do
-    for col in "${cols[@]}"
-    do
-        for partition_type in "${partition_types[@]}"
-        do
-            for row_split in "${row_splits[@]}"
-            do
-                for col_split in "${col_splits[@]}"
-                do
-                    for percentage_of_block in "${percentage_of_blocks[@]}"
-                    do
-                        for percentage_of_zero in "${percentage_of_zeros[@]}"
-                        do
-                            python gen.py --num-rows $row --num-cols $col --partition-type $partition_type --row-split $row_split --col-split $col_split --percentage-of-blocks $percentage_of_block --percentage-of-zeros $percentage_of_zero &
-                        done
-                    done
-                    # https://stackoverflow.com/questions/356100/how-to-wait-in-bash-for-several-subprocesses-to-finish-and-return-exit-code-0
-                    # wait for the processes to finish
-                    # the number of processes to wait for is the number of elements in the percentage of zeros array times the number of elements in the percentage of blocks array
-                    for job in `jobs -p`
-                    do
-                        echo $job
-                        wait $job || let "FAIL+=1"
-                    done
-                    echo $FAIL
-                    if [ "$FAIL" == "0" ];
-                    then
-                        echo "YAY!"
-                    else
-                        echo "FAIL! ($FAIL)"
-                    fi
-                    echo "Done with $row $col $partition_type $row_split $col_split"
-                done
-            done
-        done
-    done
-done
+parallel -j $jobs python gen.py --num-rows {1} --num-cols {2} --partition-type {3} --row-split {4}  --col-split {5} --percentage-of-blocks {6} --percentage-of-zeros {7} \
+                                ::: $rows      ::: $cols      ::: $partition_types ::: $row_splits  ::: $col_splits ::: $percentage_of_blocks ::: $percentage_of_zeros
 end=`date +%s.%N`
 
 runtime=$( echo "$end - $start" | bc -l )

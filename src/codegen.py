dbf00ccacfdc12092e1da40263a5643639f207c4
Pratyush Das <reikdas@gmail.com>
2024-02-14 17:47:19 -0500

    Store multiple tmp before fmadd

--- src/codegen.py
+++ src/codegen.py
@@ -263,7 +263,8 @@ int lcm(int a, int b) {
     code.append("\tgettimeofday(&t1, NULL);\n")
     code.append("\tlong t1s = t1.tv_sec * 1000000L + t1.tv_usec;\n")
     code.append("\tfloat tmp;\n")
-    code.append("\t__m512 sv, bv, cv;\n")
+    code.append("\t__m512 bv, cv;\n")
+    code.append("\t __m512 sv[10];\n")
     count = 0
     for a in range(len(rpntr)-1):
         if bpntrb[a] == -1:
@@ -272,13 +273,16 @@ int lcm(int a, int b) {
         for b in range(len(cpntr)-1):
             if b in valid_cols:
                 code.append(f"\tfor (int i={rpntr[a]}; i<{rpntr[a+1]}; i++) {{\n")
-                code.append(f"\t\tfor (int k={cpntr[b]}; k<{cpntr[b+1]}; k++) {{\n")
-                code.append(f"\t\t\ttmp=val[{indx[count]}+ (k-{cpntr[b]})*{rpntr[a+1]-rpntr[a]} + (i-{rpntr[a]})];\n")
-                code.append("\t\t\tsv=_mm512_set1_ps(tmp);\n")
+                code.append(f"\t\tfor (int k={cpntr[b]}; k<{cpntr[b+1]}; k+=10) {{\n")
+                code.append("\t\t\tfor (int n=0; n<10; n++) {\n")
+                code.append(f"\t\t\t\tsv[n]=_mm512_set1_ps(val[{indx[count]}+ (k+n-{cpntr[b]})*{rpntr[a+1]-rpntr[a]} + (i-{rpntr[a]})]);\n")
+                code.append("\t\t\t}\n")
                 code.append("\t\t\tfor (int j=0; j<512/16; j++) {\n")
                 code.append("\t\t\t\tcv = _mm512_loadu_ps(&y[i*512 + 16*j]);\n")
-                code.append("\t\t\t\tbv = _mm512_loadu_ps(&x[k*512 + 16*j]);\n")
-                code.append("\t\t\t\tcv = _mm512_fmadd_ps(sv, bv, cv);\n")
+                code.append("\t\t\t\tfor (int n=0; n<10; n++) {\n")
+                code.append("\t\t\t\t\tbv = _mm512_loadu_ps(&x[(k+n)*512 + 16*j]);\n")
+                code.append("\t\t\t\t\tcv = _mm512_fmadd_ps(sv[n], bv, cv);\n")
+                code.append("\t\t\t\t}\n")
                 code.append("\t\t\t\t_mm512_storeu_ps(&y[512*i+16*j], cv);\n")
                 code.append("\t\t\t}\n")
                 code.append("\t\t}\n")

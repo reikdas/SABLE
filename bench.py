04dba296644b3f4f8c57ad85b08c7f06b9ddc9dc
Pratyush Das <reikdas@gmail.com>
2024-02-19 10:42:51 -0500

    Unroll and jam
    Not passing tests

--- bench.py
+++ bench.py
@@ -2,11 +2,11 @@ import subprocess
 import os
 import time
 
-from src.spmv_codegen import vbr_spmv_codegen
+from src.codegen import vbr_spmv_codegen, vbr_spmm_codegen
 
 BENCHMARK_FREQ = 5
 
-if __name__ == "__main__":
+def bench_spmv():
     vbr_files = os.listdir("Generated_VBR")
     print("Benchmarking inspector")
     with open("benchmarks_inspector.txt", "w") as fInspector:
@@ -50,3 +50,29 @@ if __name__ == "__main__":
                 p = f"{fname},{','.join(execution_times)}\n"
                 print(p, flush=True)
                 fMy.write(p)
+
+def bench_spmm():
+    vbr_files = os.listdir("Generated_VBR")
+    for thread in [1]:
+        with open(f"benchmarks_my_spmm_{thread}.txt", "w") as fMy:
+            for filename in vbr_files:
+                fname = filename[:-len(".vbr")]
+                spmm_file = fname + ".c"
+                print(filename, flush=True)
+                # compile the generated code for SpMV operation
+                vbr_spmm_codegen(fname, threads=thread)
+                subprocess.run(["/usr/bin/gcc-8", "-O3", "-lpthread", "-march=native", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-o", fname, spmm_file], cwd="Generated_SpMM")
+                execution_times = []
+                for i in range(BENCHMARK_FREQ):
+                    print(f"Benchmarking threads={thread} executor iteration", i, flush=True)
+                    output = subprocess.run(["./"+fname], capture_output=True, cwd="Generated_SpMM")
+                    execution_time = output.stdout.decode("utf-8").split("\n")[0].split(" = ")[1]
+                    execution_times.append(execution_time)
+                # save execution times to file
+                p = f"{fname},{','.join(execution_times)}\n"
+                print(p, flush=True)
+                fMy.write(p)
+
+if __name__ == "__main__":
+    # bench_spmv()
+    bench_spmm()
--- testbench.py
+++ testbench.py
@@ -4,7 +4,7 @@ from src.codegen import vbr_spmm_codegen
 
 if __name__ == "__main__":
     vbr_spmm_codegen(sys.argv[1], threads=1)
-    subprocess.run(["/usr/bin/gcc-8", "-O3", "-mprefer-vector-width=512", "-mavx", "-march=native", "-o", "testbench", sys.argv[1]+".c"], cwd="Generated_SpMM")
+    subprocess.run(["/usr/bin/gcc-8", "-O3", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-march=native", "-o", "testbench", sys.argv[1]+".c"], cwd="Generated_SpMM")
     sum = 0
     for i in range(5):
         output = subprocess.check_output(["./testbench"], cwd="Generated_SpMM").decode("utf-8").split("\n")[0].split("=")[1]

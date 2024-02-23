0c1212399862eba351ef54b763a3d0ded0902d63
Adhitha Dias <kadhitha@purdue.edu>
2024-02-23 00:05:29 -0500

    add test

--- tests/test_convert_mtx_to_vbr.py
+++ tests/test_convert_mtx_to_vbr.py
@@ -62,4 +62,30 @@ def test_get_dense_for_mtx():
     plt.savefig('tests/matrices/example.png')
     
     
+def test_mtx_to_vbr_no_blk_in_rowp():
+    csr = mmread("tests/example.mtx")
+    dense = csr.toarray()
     
+    row_widths = [2 for _ in range(0, dense.shape[0]//2)]
+    col_widths = [2 for _ in range(0, dense.shape[1]//2)]
+    
+    row_widths = [2, 3, 1, 3, 2]
+    col_widths = [2, 3, 1, 3, 2]
+    
+    vbr = convert_dense_to_vbr(dense, row_widths, col_widths)
+            
+    print(vbr.val)
+    print(vbr.indx)
+    print(vbr.bindx)
+    print(vbr.rpntr)
+    print(vbr.cpntr)
+    print(vbr.bpntrb)
+    print(vbr.bpntre)
+    
+    assert vbr.val == [4.0, 1.0, 2.0, 5.0, 1.0, 2.0, -1.0, 0.0, 1.0, -1.0, 2.0, 1.0, 3.0, 4.0, 5.0, 10.0, 4.0, 3.0, 2.0, 4.0, 3.0, 0.0, 13.0, 3.0, 2.0, 4.0, 11.0, 0.0, 2.0, 3.0, 7.0, 8.0, -2.0, 4.0, 3.0, 25.0, 8.0, 3.0, 12.0]
+    assert vbr.indx == [0, 4, 6, 10, 12, 15, 16, 19, 22, 31, 35, 39]
+    assert vbr.bindx == [0, 2, 4, 0, 1, 2, 3, 2, 3, 0, 4]
+    assert vbr.rpntr == [0, 2, 5, 6, 9, 11]
+    assert vbr.cpntr == [0, 2, 5, 6, 9, 11]
+    assert vbr.bpntrb == [0, -1, 3, 7, 9]
+    assert vbr.bpntre == [3, -1, 7, 9, 11]

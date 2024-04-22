f15d91061755bf0d9df084d353d2bdc55fee710e
Pratyush Das <reikdas@gmail.com>
2024-04-22 15:12:18 -0400

    Add test for SpMV

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import os
-from src.mtx_matrices_gen import vbr_to_mtx
-from src.codegen import vbr_spmm_codegen
 import subprocess
 
 import numpy
+
+from src.codegen import vbr_spmm_codegen, vbr_spmv_codegen
+from src.mtx_matrices_gen import vbr_to_mtx
+
 
 def cmp_file(file1, file2):
     with open(file1, "r") as f1:
@@ -13,7 +15,7 @@ def cmp_file(file1, file2):
                     return False
     return True
 
-def test_spmm():
+def test_setup_file():
     filename = "example.vbr"
     dense = vbr_to_mtx(filename, dir_name="tests", vbr_dir="tests")
     dense_canon = numpy.array([[ 4.,  2.,  0.,  0.,  0.,  1.,  0.,  0.,  0., -1.,  1.],
@@ -29,9 +31,21 @@ def test_spmm():
                                 [-2.,  3.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  8., 12.]])
     assert(numpy.array_equal(dense, dense_canon))
     assert(cmp_file("tests/example.mtx", "tests/example-canon.mtx"))
-    vbr_spmm_codegen(filename="example", dir_name="tests", threads=1, vbr_dir="tests")
+
+def test_spmv():
+    test_setup_file()
+    vbr_spmv_codegen(filename="example", dense_blocks_only=True, dir_name="tests", threads=1, vbr_dir="tests")
     subprocess.check_call(["gcc", "-o", "example", "example.c", "-march=native", "-O3"], cwd="tests")
     output = subprocess.check_output(["./example"], cwd="tests").decode("utf-8").split("\n")[1:]
     with open(os.path.join("tests", "output.txt"), "w") as f:
         f.write("\n".join(output))
-    assert(cmp_file("tests/output.txt", "tests/output-canon.txt"))
+    assert(cmp_file("tests/output.txt", "tests/output_spmv_canon.txt"))
+
+def test_spmm():
+    test_setup_file()
+    vbr_spmm_codegen(filename="example", dense_blocks_only=True, dir_name="tests", threads=1, vbr_dir="tests")
+    subprocess.check_call(["gcc", "-o", "example", "example.c", "-march=native", "-O3"], cwd="tests")
+    output = subprocess.check_output(["./example"], cwd="tests").decode("utf-8").split("\n")[1:]
+    with open(os.path.join("tests", "output.txt"), "w") as f:
+        f.write("\n".join(output))
+    assert(cmp_file("tests/output.txt", "tests/output_spmm_canon.txt"))

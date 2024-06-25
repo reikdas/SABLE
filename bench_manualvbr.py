1d0398394712d18d0b878a15e2b68ee5b23fa7b5
Pratyush Das <reikdas@gmail.com>
2024-06-25 22:28:23 +0000

    Bench inspection times + generate vbr from mtx

--- bench_manualvbr.py
+++ bench_manualvbr.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import time
 
 from src.codegen import vbr_spmv_codegen, vbr_spmm_codegen
 from src.fileio import write_dense_matrix, write_dense_vector
@@ -10,6 +11,31 @@ def bench_spmv():
     vbr_dir = "manual_vbr"
     codegen_dir = "Generated_SpMV_manual"
     vbr_files = os.listdir(vbr_dir)
+    print("Benchmarking inspector")
+    with open(os.path.join("results", "benchmarks_inspector_spmv_manual.csv"), "w") as fInspector:
+        for filename in vbr_files:
+            fname = filename[:-len(".vbr")]
+            spmv_file = fname + ".c"
+            print(filename, flush=True)
+            inspector_times = []
+            vbr_spmv_codegen(fname, density=1, dir_name=codegen_dir, vbr_dir=vbr_dir, threads=1)
+            subprocess.run(["gcc", "-O3", "-funroll-all-loops", "-march=native", "-o", fname, spmv_file], cwd=codegen_dir)
+            for i in range(BENCHMARK_FREQ):
+                # SpMV code generation by inspecting the VBR matrix
+                print("Benchmarking inspector iteration", i, flush=True)
+                spmv_codegen_time = vbr_spmv_codegen(fname, density=1, dir_name=codegen_dir, vbr_dir=vbr_dir, threads=1)
+                time1 = time.time_ns() // 1_000
+                # compile the generated code for SpMV operation
+                subprocess.run(["gcc", "-O3", "-funroll-all-loops", "-march=native", "-o", fname, spmv_file], cwd=codegen_dir)
+                time2 = time.time_ns() // 1_000
+                compilation_time = time2 - time1
+                inspector_time = spmv_codegen_time + compilation_time
+                inspector_times.append(inspector_time)
+            # save inspector time (code generation + compilation) to file
+            p = f"{fname},{','.join([str(x) for x in inspector_times])}\n"
+            print(p, flush = True)
+            fInspector.write(p)
+    print("Benchmarking executor")
     for thread in [1, 2, 4, 8, 16]:
         with open(os.path.join("results", f"benchmarks_spmv_manual_{thread}.csv"), "w") as fMy:
             for filename in vbr_files:
@@ -36,6 +62,31 @@ def bench_spmm():
     vbr_dir = "manual_vbr"
     codegen_dir = "Generated_SpMM_manual"
     vbr_files = os.listdir(vbr_dir)
+    print("Benchmarking inspector")
+    with open(os.path.join("results", "benchmarks_inspector_spmm_manual.csv"), "w") as fInspector:
+        for filename in vbr_files:
+            fname = filename[:-len(".vbr")]
+            spmm_file = fname + ".c"
+            print(filename, flush=True)
+            inspector_times = []
+            vbr_spmm_codegen(fname, density=0, dir_name=codegen_dir, vbr_dir=vbr_dir, threads=1)
+            subprocess.run(["gcc", "-O3", "-march=native", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-o", fname, spmm_file], cwd=codegen_dir)
+            for i in range(BENCHMARK_FREQ):
+                # SpMV code generation by inspecting the VBR matrix
+                print("Benchmarking inspector iteration", i, flush=True)
+                spmm_codegen_time = vbr_spmm_codegen(fname, density=1, dir_name=codegen_dir, vbr_dir=vbr_dir, threads=1)
+                time1 = time.time_ns() // 1_000
+                # compile the generated code for SpMV operation
+                subprocess.run(["gcc", "-O3", "-march=native", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-o", fname, spmm_file], cwd=codegen_dir)
+                time2 = time.time_ns() // 1_000
+                compilation_time = time2 - time1
+                inspector_time = spmm_codegen_time + compilation_time
+                inspector_times.append(inspector_time)
+            # save inspector time (code generation + compilation) to file
+            p = f"{fname},{','.join([str(x) for x in inspector_times])}\n"
+            print(p, flush = True)
+            fInspector.write(p)
+    print("Benchmarking executor")
     for thread in [1, 2, 4, 8, 16]:
         with open(os.path.join("results", f"benchmarks_spmm_manual_{thread}.csv"), "w") as fMy:
             for filename in vbr_files:

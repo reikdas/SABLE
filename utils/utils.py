10f9c2ee32ba788c2de81b4fb79a693f610b8f55
Pratyush Das <reikdas@gmail.com>
2025-01-27 15:58:05 -0500

    Proper parallel benchmarking

---
+++ utils/utils.py
@@ -0,0 +1,24 @@
+import os
+
+def check_file_matches_parent_dir(filepath):
+    """
+    Check if a file's name (without suffix) matches its parent directory name.
+    
+    Args:
+        filepath (str): Full path to the file
+        
+    Returns:
+        bool: True if file name (without suffix) matches parent directory name
+        
+    Example:
+        >>> path = '/local/scratch/a/das160/SABLE/Suitesparse/GD96_a/GD96_a.mtx'
+        >>> check_file_matches_parent_dir(path)
+        True
+    """
+    # Get the file name without extension
+    file_name = os.path.splitext(os.path.basename(filepath))[0]
+    
+    # Get the parent directory name
+    parent_dir = os.path.basename(os.path.dirname(filepath))
+    
+    return file_name == parent_dir

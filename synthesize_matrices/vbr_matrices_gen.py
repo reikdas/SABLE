2f30dcfc780c3c2b74e06a79122e659a9865b28a
Pratyush Das <reikdas@gmail.com>
2025-02-03 16:11:20 -0500

    Fix type signature

--- synthesize_matrices/vbr_matrices_gen.py
+++ synthesize_matrices/vbr_matrices_gen.py
@@ -33,7 +33,7 @@ def random_splits(n, a):
 
     return differences
 
-def vbr_matrix_gen(m: int, n: int, partitioning: str, row_split: int, col_split: int, num_dense: int, perc_dense_zeros: int, num_sparse: int, dense_blocks_only: bool, vbr_dir: str = "Generated_VBR") -> None:
+def vbr_matrix_gen(m: int, n: int, partitioning: str, row_split: int, col_split: int, num_dense: int, perc_dense_zeros: int, num_sparse: int, dense_blocks_only: bool, vbr_dir: str = "Generated_VBR") -> str:
     assert(m%row_split == 0)
     assert(n%col_split == 0)
     if partitioning == "nonuniform":
@@ -103,11 +103,11 @@ def vbr_matrix_gen(m: int, n: int, partitioning: str, row_split: int, col_split:
     vbr_matrix = VBR(val, indx, bindx, rpntr, cpntr, bpntrb, bpntre)
     
     if (dense_blocks_only):
-        filename = f"Matrix_{m}_{n}_{row_split}_{col_split}_{num_dense}_{perc_dense_zeros}_{partitioning}"
+        filename: str = f"Matrix_{m}_{n}_{row_split}_{col_split}_{num_dense}_{perc_dense_zeros}_{partitioning}"
         # Generated_VBR_Dense contains mostly dense blocks
         write_vbr_matrix(filename, vbr_matrix, vbr_dir)
     else:
-        filename = f"Matrix_{m}_{n}_{row_split}_{col_split}_{num_dense}_{perc_dense_zeros}_{num_sparse}_{partitioning}"
+        filename: str = f"Matrix_{m}_{n}_{row_split}_{col_split}_{num_dense}_{perc_dense_zeros}_{num_sparse}_{partitioning}"
         # Generated_VBR_Sparse contains matrices with some mostly sparse blocks
         write_vbr_matrix(filename, vbr_matrix, vbr_dir + "_Sparse")
 

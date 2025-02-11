fbb978d89fef8ef699ebf1a3e313d44d45294a60
magronox <abasareh@purdue.edu>
2025-02-11 17:41:21 -0500

    clean up

--- src/autopartition.py
+++ src/autopartition.py
@@ -5,7 +5,7 @@ import pathlib
 import numpy as np
 import scipy
 
-# from utils.convert_real_to_vbr import convert_sparse_to_vbr, convert_vbr_to_compressed
+from utils.convert_real_to_vbr import convert_sparse_to_vbr, convert_vbr_to_compressed
 
 FILEPATH = pathlib.Path(__file__).resolve().parent
 BASE_PATH = os.path.join(FILEPATH, "..")
@@ -50,8 +50,7 @@ def cut_indices2(A_, cut_threshold, similarity, run = 3):
                 and similarity(A[:, i ].toarray().ravel(), A[:, i + run_idx].toarray().ravel()) < cut_threshold
             ):
                 run_idx += 1
-                print(similarity(A[:, i ].toarray().ravel(), A[:, i + run_idx].toarray().ravel()) < cut_threshold
-            )
+            
             if run_idx == run:
                 col_indices.append(i+1)
             else: 
@@ -105,7 +104,7 @@ def cut_indices2(A_, cut_threshold, similarity, run = 3):
             if entered:
                 row_indices.append(i)
         run_idx = 1
-
+        
     if col_indices[-1] != A.shape[1]:
         col_indices.append(A.shape[1])
     if row_indices[-1] != A.shape[0]:
@@ -172,20 +171,3 @@ def my_convert_dense_to_vbr(file_info, cut_threshold, cut_indices, similarity):
 # if __name__ == "__main__":
 #     # partition_dlmc("Real_mtx", "Real_vbr")
 #     partition_suitesparse()
-
-if __name__ == "__main__":
-    cut_indices = cut_indices2
-    similarity = similarity2
-    dense = np.array([[ 4.,  2.,  0.,  0.,  0.,  1.,  0.,  0.,  0., -1.,  1.],
-                                [ 1.,  5.,  0.,  0.,  0.,  2.,  0.,  0.,  0.,  0., -1.],
-                                [ 0.,  0.,  6.,  1.,  2.,  2.,  0.,  0.,  0.,  0.,  0.],
-                                [ 0.,  0.,  2.,  7.,  1.,  0.,  0.,  0.,  0.,  0.,  0.],
-                                [ 0.,  0., -1.,  2.,  9.,  3.,  0.,  0.,  0.,  0.,  0.],
-                                [ 2.,  1.,  3.,  4.,  5., 10.,  4.,  3.,  2.,  0.,  0.],
-                                [ 0.,  0.,  0.,  0.,  0.,  4., 13.,  4.,  2.,  0.,  0.],
-                                [ 0.,  0.,  0.,  0.,  0.,  3.,  3., 11.,  3.,  0.,  0.],
-                                [ 0.,  0.,  0.,  0.,  0.,  0.,  2.,  0.,  7.,  0.,  0.],
-                                [ 8.,  4.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 25.,  3.],
-                                [-2.,  3.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  8., 12.]])
-    A = scipy.sparse.csc_matrix(dense)
-    cpntr, rpntr = cut_indices(A, 0.2, similarity)

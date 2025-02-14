4fb4389c9416310e615d2161b6690108c6aec393
Anmol Sahoo <anmol.sahoo25@gmail.com>
2025-02-14 16:28:46 -0500

    added new conversion function to speed up VBR conversion

--- utils/convert_real_to_vbr.py
+++ utils/convert_real_to_vbr.py
@@ -1,10 +1,17 @@
 import gc
 import os
 import pathlib
+from collections import namedtuple
 
 import numpy
+import numpy as np
 import scipy
 from scipy.io import mmread
+from tqdm import tqdm
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 FILEPATH = pathlib.Path(__file__).resolve().parent
 BASE_PATH = os.path.join(FILEPATH, "..")
@@ -61,7 +68,7 @@ def convert_vbr_to_compressed(val, rpntr, cpntr, indx, bindx, bpntrb, bpntre, de
         f.write(f"coo_j=[{','.join(map(str, coo_j))}]\n")
     return val2, indx2, bindx, bpntrb, bpntre, ublocks, coo_i, coo_j, coo_val
 
-def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir):
+def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir, savez = False):
     '''
     Converts a dense matrix to a VBR matrix.
     Example:
@@ -132,14 +139,139 @@ def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir):
             bpntrb.append(-1)
             bpntre.append(-1)
     
-    with open(os.path.join(dst_dir, f"{fname}.vbr"), "w") as f:
-        f.write(f"val=[{','.join(map(str, val))}]\n")
-        f.write(f"indx=[{','.join(map(str, indx))}]\n")
-        f.write(f"bindx=[{','.join(map(str, bindx))}]\n")
-        f.write(f"rpntr=[{','.join(map(str, rpntr))}]\n")
-        f.write(f"cpntr=[{','.join(map(str, cpntr))}]\n")
-        f.write(f"bpntrb=[{','.join(map(str, bpntrb))}]\n")
-        f.write(f"bpntre=[{','.join(map(str, bpntre))}]\n")
+    out_path = os.path.join(dst_dir, f"{fname}.vbr")
+
+    if savez:
+        np.savez_compressed(
+            out_path,
+            val = val,
+            indx = indx, 
+            bindx = bindx,
+            rpntr = rpntr,
+            cpntr = cpntr,
+            bpntrb = bpntrb,
+            bpntre = bpntre)
+    else:
+        with open(os.path.join(dst_dir, f"{fname}.vbr"), "w") as f:
+            f.write(f"val=[{','.join(map(str, val))}]\n")
+            f.write(f"indx=[{','.join(map(str, indx))}]\n")
+            f.write(f"bindx=[{','.join(map(str, bindx))}]\n")
+            f.write(f"rpntr=[{','.join(map(str, rpntr))}]\n")
+            f.write(f"cpntr=[{','.join(map(str, cpntr))}]\n")
+            f.write(f"bpntrb=[{','.join(map(str, bpntrb))}]\n")
+            f.write(f"bpntre=[{','.join(map(str, bpntre))}]\n")
+
+    return val, indx, bindx, bpntrb, bpntre
+
+def convert_sparse_to_vbr_np(mat, rpntr, cpntr, fname, dst_dir, savez = False):
+    '''
+    Converts a dense matrix to a VBR matrix.
+    Example:
+    Inputs:
+    dense = [
+        [ 4.  2. | 0.  0.  0. | 1. | 0.  0.  0. |-1.  1.]
+        [ 1.  5. | 0.  0.  0. | 2. | 0.  0.  0. | 0. -1.]
+        -------------------------------------------------
+        [ 0.  0. | 6.  1.  2. | 2. | 0.  0.  0. | 0.  0.]
+        [ 0.  0. | 2.  7.  1. | 0. | 0.  0.  0. | 0.  0.]
+        [ 0.  0. |-1.  2.  9. | 3. | 0.  0.  0. | 0.  0.]
+        -------------------------------------------------
+        [ 2.  1. | 3.  4.  5. |10. | 4.  3.  2. | 0.  0.]
+        -------------------------------------------------
+        [ 0.  0. | 0.  0.  0. | 4. |13.  4.  2. | 0.  0.]
+        [ 0.  0. | 0.  0.  0. | 3. | 3. 11.  3. | 0.  0.]
+        [ 0.  0. | 0.  0.  0. | 0. | 2.  0.  7. | 0.  0.]
+        -------------------------------------------------
+        [ 8.  4. | 0.  0.  0. | 0. | 0.  0.  0. |25.  3.]
+        [-2.  3. | 0.  0.  0. | 0. | 0.  0.  0. | 8. 12.]
+    ]
+    Returns:
+    VBR(val=[4.0, 1.0, 2.0, 5.0, 1.0, 2.0, -1.0, 0.0, 1.0, -1.0, 6.0, 2.0, -1.0, 1.0, 7.0, 2.0, 2.0, 1.0, 9.0, 2.0, 0.0, 3.0, 2.0, 1.0, 3.0, 4.0, 5.0, 10.0, 4.0, 3.0, 2.0, 4.0, 3.0, 0.0, 13.0, 3.0, 2.0, 4.0, 11.0, 0.0, 2.0, 3.0, 7.0, 8.0, -2.0, 4.0, 3.0, 25.0, 8.0, 3.0, 12.0],
+        indx=[0, 4, 6, 10, 19, 22, 24, 27, 28, 31, 34, 43, 47, 51],
+        bindx=[0, 2, 4, 1, 2, 0, 1, 2, 3, 2, 3, 0, 4],
+        rpntr=[0, 2, 5, 6, 9, 11],
+        cpntr=[0, 2, 5, 6, 9, 11],
+        bpntrb=[0, 3, 5, 9, 11],
+        bpntre=[3, 5, 9, 11, 13])
+    '''
+    Sub_block = namedtuple('Sub_block', ['v', 'r_start', 'r_end', 'c_start', 'c_end', 'nnz', 'size', 'frac', 'idx_in_row'])
+
+    val = None
+    indx = None
+    bindx = None
+    bpntrb = None
+    bpntre = None
+    sub_blocks = []
+
+    logger.info(f"mat shape: {mat.shape}, num block rows: {len(rpntr)}, num block cols : {len(cpntr)}, nnz: {mat.nnz}")
+
+    for r_i in tqdm(range(len(rpntr) - 1)):
+        for c_i in range(len(cpntr) - 1):
+            r_start, r_end = rpntr[r_i], rpntr[r_i + 1]
+            c_start, c_end = cpntr[c_i], cpntr[c_i + 1]
+            logger.debug(f"block idxs at: {r_start}, {r_end}, {c_start}, {c_end}")
+
+            # extract the block 
+            block = mat[r_start:r_end, c_start:c_end]
+            block_sx, block_sy = block.shape
+            nnz = block.nnz
+            size = block_sx * block_sy
+            idx_in_row = c_i
+
+            # check nnz, if nnz greater than zero, then create a new block
+            if nnz > 0:
+                sub_blocks.append(Sub_block(block, r_start, r_end, c_start, c_end, nnz, size, nnz / size, idx_in_row))
+            else:
+                pass
+
+    # create vals
+    val = np.concat(list(map(lambda x: x.v.todense().flatten(order = 'F'), sub_blocks)), axis = 1).A1
+    logger.info(f"created vals with length: {val.shape}")
+
+    # create indx
+    indx = np.concat([[0], np.cumsum(list(map(lambda x: x.size, sub_blocks)))])
+
+    logger.info(f"created indx with length: {indx.shape}")
+    logger.debug(f"created indx: {indx}")
+
+    # create bindx
+    bindx = np.array(list(map(lambda x: x.idx_in_row, sub_blocks)))
+
+    logger.info(f"created bindx with length: {bindx.shape}")
+    logger.debug(f"created bindx: {bindx}")
+
+    # create bpntrb and bpntre
+    block_counts_per_row = np.cumsum(np.unique(list(map(lambda x: x.r_start, sub_blocks)), return_counts = True)[1])
+    bpntrb = np.concat([[0], block_counts_per_row[:-1]])
+    bpntre = block_counts_per_row
+
+    logger.info(f"created bpntrb, bpntre with lengths: {bpntrb.shape},{bpntre.shape}")
+    logger.debug(f"created bpntrb, bpntre: {bpntrb},{bpntre}")
+
+    # save to file
+
+    out_path = os.path.join(dst_dir, f"{fname}.vbr")
+
+    if savez:
+        np.savez_compressed(
+            out_path,
+            val = val,
+            indx = indx, 
+            bindx = bindx,
+            rpntr = rpntr,
+            cpntr = cpntr,
+            bpntrb = bpntrb,
+            bpntre = bpntre)
+    else:
+        with open(os.path.join(dst_dir, f"{fname}.vbr"), "w") as f:
+            f.write(f"val=[{','.join(map(str, val))}]\n")
+            f.write(f"indx=[{','.join(map(str, indx))}]\n")
+            f.write(f"bindx=[{','.join(map(str, bindx))}]\n")
+            f.write(f"rpntr=[{','.join(map(str, rpntr))}]\n")
+            f.write(f"cpntr=[{','.join(map(str, cpntr))}]\n")
+            f.write(f"bpntrb=[{','.join(map(str, bpntrb))}]\n")
+            f.write(f"bpntre=[{','.join(map(str, bpntre))}]\n")
+
     return val, indx, bindx, bpntrb, bpntre
 
 if __name__ == "__main__":

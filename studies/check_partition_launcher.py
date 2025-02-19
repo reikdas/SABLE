c685d8e01033903c054473b87e6f4137762dff75
Pratyush Das <reikdas@gmail.com>
2025-02-18 22:30:21 -0500

    Some clean up

--- studies/check_partition_launcher.py
+++ studies/check_partition_launcher.py
@@ -3,6 +3,8 @@ import pathlib
 import subprocess
 import sys
 
+import scipy
+
 FILEPATH = pathlib.Path(__file__).resolve().parent.parent
 
 # Import hack - is there an alternative?
@@ -14,7 +16,9 @@ FILEPATH = pathlib.Path(__file__).resolve().parent
 BASE_PATH = os.path.join(FILEPATH, "..")
 
 matrices = [
-    "orbitRaising_2",
+    "heart1",
+    "nemsemm1",
+    "bcsstk36",
 ]
 skip = [
     "xenon1", "invextr1_new", "majorbasis", "gyro_k",
@@ -37,10 +41,15 @@ if __name__ == "__main__":
     for file_path in mtx_dir.rglob("*"):
         if file_path.is_file() and file_path.suffix == ".mtx" and check_file_matches_parent_dir(file_path):
             fname = pathlib.Path(file_path).resolve().stem
-            # if fname not in matrices:
-            #     continue
-            if fname in skip:
+            if fname not in matrices:
                 continue
+            # if fname in skip:
+            #     continue
+            # mtx = scipy.io.mmread(file_path)
+            # if mtx.nnz < 1_000_000:
+            #     continue
+            # if mtx.nnz > 10_000_000:
+            #     continue
             files.append(file_path)
     pid = os.getpid()
     cpu_affinity = os.sched_getaffinity(pid)
@@ -59,3 +68,12 @@ if __name__ == "__main__":
             f.write("\n")
             start = end
     subprocess.run(["mpirun", "--cpu-list", ",".join(map(str, cpu_affinity)), "--bind-to", "cpu-list:ordered", "-np", str(num_cores), "python3", "check_partition.py"], check=True, env=dict(os.environ) | {"HWLOC_COMPONENTS": "-gl"}, cwd=os.path.join(BASE_PATH, "studies"))
+    with open(os.path.join(BASE_PATH, "studies", "stats.csv"), "w") as f:
+        f.write("Matrix,nnz,extra nnz,num blocks,num dense blocks,old density,new density\n")
+        for core in range(num_cores):
+            with open(os.path.join(BASE_PATH, "studies", f"stats_dist_{core}.csv")) as g:
+                for line in g:
+                    if not line.startswith("Matrix,"):
+                        f.write(line)
+            os.remove(os.path.join(BASE_PATH, "studies", f"stats_dist_{core}.csv"))
+    os.remove(os.path.join(BASE_PATH, "studies", "stats_dist.csv"))

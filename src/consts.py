31b77726c15cc7bc0251f681196dfdd8e9c72ce9
Pratyush Das <reikdas@gmail.com>
2025-02-24 17:48:18 -0500

    Parallel benchmarks of baselines

--- src/consts.py
+++ src/consts.py
@@ -1,2 +1,8 @@
+import os
+
+BASE_PATH = os.path.join(os.path.dirname(__file__))
+
+MKL_PATH = os.path.join(BASE_PATH, "..", "intel", "oneapi", "mkl", "latest")
+MKL_FLAGS = [f"-I{MKL_PATH}/include", f"-L{MKL_PATH}/lib/intel64", "-lmkl_rt"]
 CFLAGS = ["-O3", "-march=native", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-ffast-math", "-lpthread", "-fopenmp"]
 SPEEDUP_THRESH = 4.3

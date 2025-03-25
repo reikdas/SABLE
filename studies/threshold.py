86fce19e7867feb6cbe423a937ce38318542443a
Pratyush Das <reikdas@gmail.com>
2025-03-25 13:09:35 -0400

    Update threshold script

--- studies/threshold.py
+++ studies/threshold.py
@@ -13,6 +13,7 @@ sys.path.append(str(FILEPATH.parent))
 from src.baseline import *
 from src.codegen import *
 from src.consts import CFLAGS as CFLAGS
+from src.consts import MKL_FLAGS as MKL_FLAGS
 from synthesize_matrices.vbr_matrices_gen import vbr_matrix_gen
 from utils.convert_real_to_vbr import convert_vbr_to_compressed
 from utils.fileio import read_vbr, write_dense_vector
@@ -20,7 +21,7 @@ from utils.utils import extract_mul_nums
 
 FILEPATH=pathlib.Path(__file__).resolve().parent
 
-BENCHMARK_FREQ = 5
+BENCHMARK_FREQ = 100
 COMPILE_TIMEOUT = 60 * 60 * 3
 
 DENSE_CODEGEN_DIR = "Generated_SpMV_threshold_Dense"
@@ -32,7 +33,7 @@ def calculate_threshold():
     pid = os.getpid()
     core = psutil.Process(pid).cpu_num()
     perc_zeros_list = [0, 20, 40, 50, 75, 80, 85, 90, 95, 99]
-    dims = [1, 2, 4, 5, 8, 10, 16, 20, 25, 40, 50, 80, 100, 125, 200, 250, 400]
+    dims = [1, 2, 4, 5, 8, 10, 16, 20, 25, 40, 50, 80, 100, 125, 200, 250, 400, 500, 625, 1000, 1250, 2000, 2500, 5000]
     mat_side = 10000
     assert (mat_side % dim == 0 for dim in dims)
     write_dense_vector(1.0, mat_side)
@@ -46,7 +47,7 @@ def calculate_threshold():
                     val, indx, bindx, rpntr, cpntr, bpntrb, bpntre = read_vbr(os.path.join(DENSE_VBR_DIR, fname+".vbr"))
                     # print(fname)
                     convert_vbr_to_compressed(val, rpntr, cpntr, indx, bindx, bpntrb, bpntre, fname, DENSE_VBR_DIR, 0)
-                    vbr_spmv_codegen(fname, dir_name=DENSE_CODEGEN_DIR, vbr_dir=DENSE_VBR_DIR, threads=1, bench=50)
+                    vbr_spmv_codegen(fname, dir_name=DENSE_CODEGEN_DIR, vbr_dir=DENSE_VBR_DIR, threads=1, bench=BENCHMARK_FREQ)
                     try:
                         subprocess.run(["taskset", "-a", "-c", str(core), "gcc", f"{fname}.c", "-o", fname] + CFLAGS, cwd=DENSE_CODEGEN_DIR, check=True, timeout=COMPILE_TIMEOUT)
                     except subprocess.TimeoutExpired:
@@ -63,14 +64,14 @@ def calculate_threshold():
                     fname2: str = vbr_matrix_gen(dim1, dim2, "uniform", 1, 1, 1, perc_zeros, 0, True, SPARSE_VBR_DIR)
                     val, indx, bindx, rpntr, cpntr, bpntrb, bpntre = read_vbr(os.path.join(SPARSE_VBR_DIR, fname2+".vbr"))
                     convert_vbr_to_compressed(val, rpntr, cpntr, indx, bindx, bpntrb, bpntre, fname2, SPARSE_VBR_DIR, 100)
-                    vbr_spmv_codegen(fname2, dir_name=SPARSE_CODEGEN_DIR, vbr_dir=SPARSE_VBR_DIR, threads=1, bench=50)
+                    vbr_spmv_codegen(fname2, dir_name=SPARSE_CODEGEN_DIR, vbr_dir=SPARSE_VBR_DIR, threads=1, bench=BENCHMARK_FREQ)
                     try:
-                        subprocess.run(["taskset", "-a", "-c", str(core), "gcc", f"{fname2}.c", "-o", fname2] + CFLAGS, cwd=SPARSE_CODEGEN_DIR, check=True, timeout=COMPILE_TIMEOUT)
+                        subprocess.run(["taskset", "-a", "-c", str(core), "gcc", f"{fname2}.c", "-o", fname2] + CFLAGS + MKL_FLAGS, cwd=os.path.join(FILEPATH, SPARSE_CODEGEN_DIR), check=True, timeout=COMPILE_TIMEOUT)
                     except subprocess.TimeoutExpired:
                         print("SABLE Sparse: Compilation failed for ", fname2)
                         continue
                     try:
-                        output = subprocess.check_output(["taskset", "-a", "-c", str(core), f"./{fname2}"], cwd=SPARSE_CODEGEN_DIR).decode("utf-8").split("\n")[0]
+                        output = subprocess.check_output(["taskset", "-a", "-c", str(core), f"./{fname2}"], cwd=os.path.join(FILEPATH, SPARSE_CODEGEN_DIR)).decode("utf-8").split("\n")[0]
                     except subprocess.CalledProcessError as e:
                         print("SABLE Sparse: Execution failed for ", fname2, " with ", e)
                         continue
@@ -79,6 +80,10 @@ def calculate_threshold():
                     f.write(",".join([str(dim1), str(dim2), str(perc_zeros), str(nnz), str(median_sable_time_sparse), str(median_sable_time_dense)]))
                     f.write("\n")
                     f.flush()
+                    os.remove(os.path.join(DENSE_VBR_DIR, fname+".vbr"))
+                    os.remove(os.path.join(DENSE_VBR_DIR, fname+".vbrc"))
+                    os.remove(os.path.join(SPARSE_VBR_DIR, fname2+".vbr"))
+                    os.remove(os.path.join(SPARSE_VBR_DIR, fname2+".vbrc"))
 
 if __name__ == "__main__":
     calculate_threshold()

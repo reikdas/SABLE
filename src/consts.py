b7f0aa920b177b958dd29d12429aa757dab7c4f7
Pratyush Das <reikdas@gmail.com>
2025-04-10 19:31:01 -0400

    Prepare to run on Milind servers

--- src/consts.py
+++ src/consts.py
@@ -2,7 +2,7 @@ import os
 
 BASE_PATH = os.path.join(os.path.dirname(__file__), "..")
 
-MKL_PATH = os.path.join(BASE_PATH, "..", "intel", "oneapi", "mkl", "latest")
+MKL_PATH = os.path.join("/home", "min", "a", "das160", "intel", "oneapi", "mkl", "latest")
 MKL_FLAGS = [f"-I{MKL_PATH}/include", f"-L{MKL_PATH}/lib/intel64", "-lmkl_rt"]
 CFLAGS = ["-O3", "-march=native", "-funroll-all-loops", "-mprefer-vector-width=512", "-mavx", "-ffast-math", "-lpthread", "-fopenmp"]
 SPEEDUP_THRESH = 1.5

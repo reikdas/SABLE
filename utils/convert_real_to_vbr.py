f0a4d7543b8ef7e129580e470a318fa300ba39e3
Pratyush Das <reikdas@gmail.com>
2025-07-10 22:19:02 -0400

    Make VBRC conversion faster

--- utils/convert_real_to_vbr.py
+++ utils/convert_real_to_vbr.py
@@ -1,12 +1,13 @@
 import logging
 import os
 import pathlib
-from collections import namedtuple
+from typing import List, Optional, Tuple, Union
 
 import joblib
 import numpy
 import scipy
 from scipy.io import mmread
+from scipy.sparse import spmatrix
 
 from src.consts import *
 
@@ -15,72 +16,117 @@ logger = logging.getLogger(__name__)
 FILEPATH = pathlib.Path(__file__).resolve().parent
 BASE_PATH = os.path.join(FILEPATH, "..")
 
-def convert_vbr_to_compressed(val, rpntr, cpntr, indx, bindx, bpntrb, bpntre, fname, dst_dir, density=None):
+
+
+def convert_sparse_to_vbrc(
+    mat: spmatrix, 
+    rpntr: List[int], 
+    cpntr: List[int], 
+    fname: str, 
+    dst_dir: str, 
+    density: Optional[float] = None
+) -> Tuple[List[float], List[int], List[int], List[int], List[int], List[int], List[int], List[int], List[float]]:
+    # Load ML model if density is None
     if density is None:
         model = joblib.load(os.path.join(BASE_PATH, "models", "density_threshold_spmv.pkl"))
-    val2: list[float] = []
-    indx2: list[int] = [0]
-    ublocks: list[int] = []
-    coo_i: list[int] = []
-    coo_j: list[int] = []
-    coo_val: list[float] = []
-    count: int = 0
-    for a in range(len(rpntr)-1):
-        if bpntrb[a] == -1:
-            continue
-        valid_cols = bindx[bpntrb[a]:bpntre[a]]
-        for b in range(len(cpntr)-1):
-            if b in valid_cols:
-                sparse_count = 0
-                count2 = 0
+    
+    val2: List[float] = []
+    indx2: List[int] = [0]
+    bindx: List[int] = []
+    bpntrb: List[int] = []
+    bpntre: List[int] = []
+    ublocks: List[int] = []
+    coo_i: List[int] = []
+    coo_j: List[int] = []
+    coo_val: List[float] = []
+    
+    block_count = 0
+    
+    for r_i in range(len(rpntr) - 1):
+        row_start_block = block_count
+        r_start, r_end = rpntr[r_i], rpntr[r_i + 1]
+        
+        for c_i in range(len(cpntr) - 1):
+            c_start, c_end = cpntr[c_i], cpntr[c_i + 1]
+            
+            # Extract the block
+            block = mat[r_start:r_end, c_start:c_end]
+            nnz = block.nnz
+            
+            # Only process non-empty blocks
+            if nnz > 0:
+                block_sx, block_sy = block.shape
+                size = block_sx * block_sy
+                
+                # Calculate density and decide whether to unroll
                 dense_elems = []
                 idxs_i = []
                 idxs_j = []
-                for idx_j in range(cpntr[b], cpntr[b+1]):
-                    for idx_i in range(rpntr[a], rpntr[a+1]):
-                        if val[indx[count]+count2] == 0.0:
-                            sparse_count+=1
-                        else:
-                            dense_elems += [val[indx[count]+count2]]
+                
+                # Extract non-zero elements and their indices
+                for idx_j in range(c_start, c_end):
+                    for idx_i in range(r_start, r_end):
+                        val = block[idx_i - r_start, idx_j - c_start]
+                        if val != 0.0:
+                            dense_elems.append(val)
                             idxs_i.append(idx_i)
                             idxs_j.append(idx_j)
-                        count2+=1
+                
                 dense_count = len(dense_elems)
-                assert(dense_count + sparse_count == (rpntr[a+1] - rpntr[a])*(cpntr[b+1] - cpntr[b]))
-                calc_density = (dense_count/(dense_count + sparse_count))*100
+                calc_density = (dense_count / size) * 100
+                
+                # Decision logic
                 if density is not None:
-                    unroll = True
-                    if calc_density > density:
-                        unroll = False
+                    unroll = calc_density <= density
                 else:
-                    unroll = True
-                    # if model.predict([[(rpntr[a+1]-rpntr[a]), (cpntr[b+1]-cpntr[b]), dense_count, calc_density]])[0] == 1:
-                    if model.predict([[(rpntr[a+1]-rpntr[a]), (cpntr[b+1]-cpntr[b]), calc_density]])[0] == 1:
-                        #print(f"{rpntr[a+1]-rpntr[a]},{cpntr[b+1]-cpntr[b]},{calc_density}")
-                        unroll = False
+                    unroll = model.predict([[block_sx, block_sy, calc_density]])[0] == 0
+                
                 if not unroll:
-                    val2.extend(val[indx[count]:indx[count+1]])
+                    # Keep as dense block
+                    block_vals = block.todense().flatten(order='F').A1
+                    val2.extend(block_vals)
                     indx2.append(len(val2))
+                    bindx.append(c_i)
                 else:
+                    # Unroll to CSR
                     coo_val.extend(dense_elems)
-                    ublocks.append(count)
+                    ublocks.append(block_count)
                     coo_i.extend(idxs_i)
                     coo_j.extend(idxs_j)
-                count+=1
+                    bindx.append(c_i)
+                
+                block_count += 1
+        
+        # Update row pointers
+        if row_start_block < block_count:
+            bpntrb.append(row_start_block)
+            bpntre.append(block_count)
+        else:
+            # Empty row - mark with -1
+            bpntrb.append(-1)
+            bpntre.append(-1)
+    
+    # Create CSR representation for unrolled blocks
     if len(coo_i) > 0:
         if (rpntr[-1]-1) not in coo_i or (cpntr[-1]-1) not in coo_j:
             coo_i.append(rpntr[-1]-1)
             coo_j.append(cpntr[-1]-1)
             coo_val.append(0.0)
         csr = scipy.sparse.coo_array((coo_val, (coo_i, coo_j))).tocsr()
-        indptr = csr.indptr
+        indptr = csr.indptr.tolist()
         assert(len(indptr) == (rpntr[-1]+1))
-        indices = csr.indices
-        csr_val = csr.data
+        indices = csr.indices.tolist()
+        csr_val = csr.data.tolist()
     else:
         csr_val = []
         indptr = []
         indices = []
+    
+    # Ensure output directory exists
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    
+    # Write to file
     with open(os.path.join(dst_dir, f"{fname}.vbrc"), "w") as f:
         f.write(f"val=[{','.join(map(str, val2))}]\n")
         f.write(f"csr_val=[{','.join(map(str, csr_val))}]\n")
@@ -93,9 +139,12 @@ def convert_vbr_to_compressed(val, rpntr, cpntr, indx, bindx, bpntrb, bpntre, fn
         f.write(f"bpntrb=[{','.join(map(str, bpntrb))}]\n")
         f.write(f"bpntre=[{','.join(map(str, bpntre))}]\n")
         f.write(f"ublocks=[{','.join(map(str, ublocks))}]\n")
+    
     return val2, indx2, bindx, bpntrb, bpntre, ublocks, indptr, indices, csr_val
 
-def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir):
+
+
+def convert_sparse_to_vbr(mat: spmatrix, rpntr: List[int], cpntr: List[int], fname: str, dst_dir: str) -> Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray]:
     '''
     Converts a matrix to a VBR matrix. If matrix provided is sparse, should be
     csc / csr (allowing slicing). The return is a set of arrays describing all
@@ -129,62 +178,42 @@ def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir):
         bpntrb=[0, 3, 5, 9, 11],
         bpntre=[3, 5, 9, 11, 13])
     '''
-    Sub_block = namedtuple('Sub_block', ['v', 'r_start', 'r_end', 'c_start', 'c_end', 'nnz', 'size', 'frac', 'idx_in_row'])
 
-    val = None
-    indx = None
-    bindx = None
-    bpntrb = None
-    bpntre = None
-    sub_blocks = []
-
-    logger.info(f"mat shape: {mat.shape}, num block rows: {len(rpntr)}, num block cols : {len(cpntr)}, nnz: {mat.nnz}")
-
+    val: List[float] = []
+    indx: List[int] = [0]
+    bindx: List[int] = []
+    bpntrb: List[int] = []
+    bpntre: List[int] = []
+    
+    block_count = 0
+    
     for r_i in range(len(rpntr) - 1):
+        row_start_block = block_count
+        r_start, r_end = rpntr[r_i], rpntr[r_i + 1]
+        
         for c_i in range(len(cpntr) - 1):
-            r_start, r_end = rpntr[r_i], rpntr[r_i + 1]
             c_start, c_end = cpntr[c_i], cpntr[c_i + 1]
-            logger.debug(f"block idxs at: {r_start}, {r_end}, {c_start}, {c_end}")
-
-            # extract the block 
+            
+            # Extract the block
             block = mat[r_start:r_end, c_start:c_end]
-            block_sx, block_sy = block.shape
             nnz = block.nnz
-            size = block_sx * block_sy
-            idx_in_row = c_i
-
-            # check nnz, if nnz greater than zero, then create a new block
+            
+            # Only process non-empty blocks
             if nnz > 0:
-                sub_blocks.append(Sub_block(block, r_start, r_end, c_start, c_end, nnz, size, nnz / size, idx_in_row))
-            else:
-                pass
-
-    # create vals
-    val = numpy.concat(list(map(lambda x: x.v.todense().flatten(order = 'F'), sub_blocks)), axis = 1).A1
-    logger.info(f"created vals with length: {val.shape}")
-
-    # create indx
-    indx = numpy.concat([[0], numpy.cumsum(list(map(lambda x: x.size, sub_blocks)))])
-
-    logger.info(f"created indx with length: {indx.shape}")
-    logger.debug(f"created indx: {indx}")
-
-    # create bindx
-    bindx = numpy.array(list(map(lambda x: x.idx_in_row, sub_blocks)))
-
-    logger.info(f"created bindx with length: {bindx.shape}")
-    logger.debug(f"created bindx: {bindx}")
-
-    # create bpntrb and bpntre
-    block_counts_per_row = numpy.cumsum(numpy.unique(list(map(lambda x: x.r_start, sub_blocks)), return_counts = True)[1])
-    bpntrb = numpy.concat([[0], block_counts_per_row[:-1]])
-    bpntre = block_counts_per_row
-
-    assert(len(bpntrb) == len(bpntre))
-    assert(len(bpntrb) == len(rpntr)-1)
-
-    logger.info(f"created bpntrb, bpntre with lengths: {bpntrb.shape},{bpntre.shape}")
-    logger.debug(f"created bpntrb, bpntre: {bpntrb},{bpntre}")
+                block_vals = block.todense().flatten(order='F').A1
+                val.extend(block_vals)
+                indx.append(len(val))
+                bindx.append(c_i)
+                block_count += 1
+        
+        # Update row pointers
+        if row_start_block < block_count:
+            bpntrb.append(row_start_block)
+            bpntre.append(block_count)
+        else:
+            # Empty row - mark with -1
+            bpntrb.append(-1)
+            bpntre.append(-1)
 
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
@@ -199,7 +228,7 @@ def convert_sparse_to_vbr(mat, rpntr, cpntr, fname, dst_dir):
         f.write(f"bpntrb=[{','.join(map(str, bpntrb))}]\n")
         f.write(f"bpntre=[{','.join(map(str, bpntre))}]\n")
 
-    return val, indx, bindx, bpntrb, bpntre
+    return numpy.array(val), numpy.array(indx), numpy.array(bindx), numpy.array(bpntrb), numpy.array(bpntre)
 
 if __name__ == "__main__":
     d = {}
